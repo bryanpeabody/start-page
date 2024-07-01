e5196527e24c5027dfff977452301b22b29f8c27
Bryan Peabody <hi@bryanpeabody.com>
2024-07-01 12:10:41 -0400

    Clean up.

--- scripts/parse-sports.sh
+++ scripts/parse-sports.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-echo "<h2>Today's Games</h2>" > sports.html
-
 #now=`date +%m/%d`
 now="07/02"
+outfile="../sports.html"
 
 baseballicon="<i class='fa-solid fa-baseball'></i> &nbsp;&nbsp;"
 
 game=`grep $now dodgers-2024.csv | awk -F "," '{ print $4 " " $3}'`
-echo $baseballicon $game >> sports.html
+echo $baseballicon $game > $outfile
 
-echo "<br />" >> sports.html
+echo "<br />" >> $outfile
 
 game=`grep $now cubs-2024.csv | awk -F "," '{ print $4 " " $3}'`
-echo $baseballicon $game >> sports.html
+echo $baseballicon $game >> $outfile
 
-echo "<br />" >> sports.html
+echo "<br />" >> $outfile
 
 case "$(date +%a)" in
   Mon)
-    echo "<i class='fa-solid fa-medal'></i> &nbsp;&nbsp; WWE Raw 08:00pm" >> sports.html
+    echo "<i class='fa-solid fa-medal'></i> &nbsp;&nbsp; WWE Raw 08:00pm" >> $outfile
     ;;
   Fri)
-    echo "<i class='fa-solid fa-medal'></i> &nbsp;&nbsp; WWE Smackdown 08:00pm" >> sports.html
+    echo "<i class='fa-solid fa-medal'></i> &nbsp;&nbsp; WWE Smackdown 08:00pm" >> $outfile
     ;;
 esac
