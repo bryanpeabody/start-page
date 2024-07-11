50442e7950c4513d996a7aad05cd153504d4f178
Bryan Peabody <hi@bryanpeabody.com>
2024-07-11 15:39:14 -0400

    Updated scripts.

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -3,3 +3,5 @@
 cd /home/bryan/projects/start-page/scripts
 ./parse-sports.sh
 
+cd /home/bryan/projects/py-peanuts-comics-scraper
+/usr/bin/python3 daily-strips-download.py
--- scripts/sync-files.sh
+++ scripts/sync-files.sh
@@ -4,3 +4,6 @@ cp ../index.html /var/www/html
 cp ../sports.html /var/www/html
 cp -r ../images /var/www/html
 cp -r ../styles /var/www/html
+
+cd ../../py-peanuts-comics-scraper/images
+cp *.gif /var/www/html/images
