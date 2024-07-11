e31117762305f9ca6e847ae598bc938bbb338145
Bryan Peabody <hi@bryanpeabody.com>
2024-07-11 15:42:57 -0400

    New lines.

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -4,4 +4,4 @@ cd /home/bryan/projects/start-page/scripts
 ./parse-sports.sh
 
 cd /home/bryan/projects/py-peanuts-comics-scraper
-/usr/bin/python3 daily-strips-download.py
+/usr/bin/python3 ./daily-strips-download.py
